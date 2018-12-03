2dbd1e9fb9a1cc340cd4921dff1625502f611727
Irimia Gabriel <irimiagrabriel96@gmail.com>
2018-12-03 13:03:08 +0200

    modified some dependencies

---
+++ TripPlanner/Common/CqrsAutofacModule.cs
@@ -0,0 +1,56 @@
+﻿using Autofac;
+using Cqrs.Service;
+using Cqrs.Service.CommandContracts;
+using Cqrs.Service.QueryContracts;
+using Persistency.Implementations;
+using Repositories.Implementations;
+using static System.Reflection.IntrospectionExtensions;
+
+namespace Common
+{
+    /// <summary>
+    /// AutofacModule for CqrsStructure project.
+    /// </summary>
+    /// <seealso cref="Autofac.Module" />
+    public class CqrsAutofacModule : Module
+    {
+        /// <summary>
+        /// Override to add registrations to the container.
+        /// </summary>
+        /// <param name="builder">The builder through which components can be
+        /// registered.</param>
+        /// <remarks>
+        /// Note that the ContainerBuilder parameter is unique to this module.
+        /// </remarks>
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterModule<PersistencyImplementationsAutofacModule>();
+            builder.RegisterModule<RepositoryImplementationsAutofacModule>();
+
+            builder.RegisterType<ICommandDispatcher>()
+                .As<ICommandDispatcher>();
+
+            builder.RegisterType<IQueryDispatcher>()
+                .As<IQueryDispatcher>();
+
+            builder.RegisterAssemblyTypes(typeof(BusinessLayer).GetTypeInfo().Assembly)
+                .Where(t => t.Name.EndsWith("QueryResult")).AsImplementedInterfaces();
+
+            builder.RegisterAssemblyTypes(typeof(BusinessLayer).GetTypeInfo().Assembly)
+                .Where(t => t.Name.EndsWith("Command")).AsImplementedInterfaces();
+
+            builder.RegisterAssemblyTypes(typeof(BusinessLayer).GetTypeInfo().Assembly)
+                .Where(t => t.Name.EndsWith("Query")).AsImplementedInterfaces();
+
+            builder.RegisterAssemblyTypes(typeof(BusinessLayer).GetTypeInfo().Assembly)
+                .AsClosedTypesOf(typeof(ICommandHandler<>)).AsImplementedInterfaces();
+
+            builder.RegisterAssemblyTypes(typeof(BusinessLayer).GetTypeInfo().Assembly)
+                .AsClosedTypesOf(typeof(IQueryHandler<,>)).AsImplementedInterfaces();
+
+            //builder.RegisterType<UserService>()
+            //    .As<IUserService>();
+
+        }
+    }
+}
--- TripPlanner/TripPlanner.Api/Startup.cs
+++ TripPlanner/TripPlanner.Api/Startup.cs
@@ -1,34 +1,66 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using AutoMapper;
+using Common;
+using Common.Extensions;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Persistency.Implementations;
 
 namespace TripPlanner.Api
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
+
+        public Startup(IHostingEnvironment env)
         {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
         }
 
+        public IConfigurationRoot Configuration { get; }
+
+        public IServiceProvider ConfigureServices(IServiceCollection services)
+        { 
+            services.AddSingleton(Configuration);
+
+            services.AddDatabase(Configuration.GetConnectionString("BiblioTechDBConnectionString"));
+            var builder = new ContainerBuilder();
+            builder.RegisterModule<CqrsAutofacModule>();
+
+            builder.Populate(services);
+            var container = builder.Build();
+
+            return container.Resolve<IServiceProvider>();
+        }
+
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        public void Configure(IApplicationBuilder app,
+                    IHostingEnvironment env,
+                    ILoggerFactory loggerFactory,
+                    TripPlannerContext context)
         {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseMvc();
         }
     }
 }
