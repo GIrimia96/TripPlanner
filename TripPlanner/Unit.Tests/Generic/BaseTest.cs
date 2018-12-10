5fd6fe6e5c6ae5f857ce78ebdecade0117849fa9
Irimia Gabriel <irimiagrabriel96@gmail.com>
2018-12-10 19:32:52 +0200

    added unit tests for employees

---
+++ TripPlanner/Unit.Tests/Generic/BaseTest.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Unit.Tests.Generic
+{
+    /// <summary>
+    /// Base Unit Test class
+    /// </summary>
+    /// <typeparam name="T">Item to Test </typeparam>
+    [TestClass]
+    public abstract class BaseTest<T>
+    {
+        protected T ItemToTest { get; set; }
+
+        [TestInitialize]
+        public virtual void SetupTest()
+        {
+            // Setup all the mocks needed for test
+            SetupMockingForTests();
+
+            // Generate ItemToTest with all the dependency
+            ItemToTest = CreateItemToTest();
+        }
+
+        /// <summary>
+        /// Create the item to test
+        /// </summary>
+        /// <returns> Item to test </returns>
+        protected abstract T CreateItemToTest();
+
+        /// <summary>
+        /// Setup mocking for the test methods
+        /// </summary>
+        protected virtual void SetupMockingForTests()
+        {
+        }
+    }
+}
---
+++ TripPlanner/Unit.Tests/Specific/CommandTest/EmployeeCommandTest/UpdateEmployeeUnitTest.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Unit.Tests.Specific.CommandTest.EmployeeCommandTest
+{
+    class UpdateEmployeeUnitTest
+    {
+    }
+}
