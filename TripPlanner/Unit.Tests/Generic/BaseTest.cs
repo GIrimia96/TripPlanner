90d76c4f97bc5c8777cf25ea1cbffa75dbceb741
Irimia Gabriel <irimiagabriel96@gmail.com>
2018-12-10 20:58:19 +0200

    updated database name

--- TripPlanner/Unit.Tests/Generic/BaseTest.cs
+++ TripPlanner/Unit.Tests/Generic/BaseTest.cs
@@ -2,10 +2,6 @@
 
 namespace Unit.Tests.Generic
 {
-    /// <summary>
-    /// Base Unit Test class
-    /// </summary>
-    /// <typeparam name="T">Item to Test </typeparam>
     [TestClass]
     public abstract class BaseTest<T>
     {
@@ -20,16 +16,9 @@ namespace Unit.Tests.Generic
             // Generate ItemToTest with all the dependency
             ItemToTest = CreateItemToTest();
         }
-
-        /// <summary>
-        /// Create the item to test
-        /// </summary>
-        /// <returns> Item to test </returns>
+        
         protected abstract T CreateItemToTest();
 
-        /// <summary>
-        /// Setup mocking for the test methods
-        /// </summary>
         protected virtual void SetupMockingForTests()
         {
         }
